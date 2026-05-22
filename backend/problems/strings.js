6ccfe6a9daff56f0e73a8d73c9a4b191802e9774
Rishabh01487 <rishabhgupta999175@gmail.com>
2026-05-23 01:30:11 +0530

    d(strings+recursion): remaining diagram refinements

--- backend/problems/strings.js
+++ backend/problems/strings.js
@@ -1055,4 +1055,4 @@ Edge cases: prefix or suffix longer than the word (immediate false), empty prefi
     sheet: "Striver A2Z",
     solution_code: "int ns=s.size(),nt=t.size(); vector<vector<int>> dp(ns+1,vector<int>(nt+1,-1)); for(int i=0;i<=ns;i++)dp[i][0]=i; for(int i=1;i<=ns;i++)for(int j=1;j<=nt;j++){if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j-1];else dp[i][j]=dp[i-1][j];} int best=1e9,pos=-1; for(int i=1;i<=ns;i++){if(dp[i][nt]!=-1){int len=i-dp[i][nt];if(len<best){best=len;pos=dp[i][nt];}}} cout<<s.substr(pos,best);",
   },
-]
+];
